957bb136c4de094f9c9c7286d39c0fb157119a46
João Galinho <joao.andre.galinho@gmail.com>
2021-04-15 22:12:05 -0400

    Initial commit

---
+++ addiction_world.py
@@ -0,0 +1,80 @@
+import numpy as np
+
+class AddictionWorld():
+    def __init__(self, max_time, n_trials, rewards=[], verbose=False):
+        '''
+        rewrads -> list of (reward, time, (first_trial, last_trial))
+        '''
+        self.max_time = max_time
+        self.n_trials = n_trials
+        self.world = np.zeros((self.n_trials, self.max_time))
+
+        for r, t, trials in rewards:
+            self.add_reward(r, t, trials, verbose=verbose)
+        
+    
+    def add_reward(self, reward, time, trials, verbose=False):
+        if time < 0 or time >= self.max_time:
+            print("Tried to add reward on time {} when max_time is {}"
+                .format(time, self.max_time))
+            return False
+        
+        if trials[0] < 0 or trials[1] >= self.n_trials-1:
+            print("Tried to add rewards for trials {}-{} when there are only trials 0-{}"
+                .format(trials[0], trials[1], self.n_trials-1))
+            return False
+        
+        if verbose:
+            print("Adding reward of {} on time {} for trials {}-{}"
+                .format(reward, time, trials[0], trials[0]))
+        for i in range(trials[0], trials[1]+1):
+            if verbose and self.world[i][time] != 0:
+                print("Replacing current reward of {} on time {} for trial {} with new reward of {}"
+                    .format(self.world[i][time], time, i, reward))
+            self.world[i][time] = reward
+        return True
+    
+
+    def clear_reward(self, time, trials, verbose=False):
+        if time < 0 or time >= self.max_time:
+            print("Tried to clear rewards on time {} when max_time is {}"
+                .format(time, self.max_time))
+            return False
+
+        if trials[0] < 0 or trials[1] >= self.n_trials-1:
+            print("Tried to clear rewards for trials {}-{} when there are only trials 0-{}"
+                .format(trials[0], trials[1], self.n_trials-1))
+            return False
+        
+        if verbose:
+            print("Clearing rewards on time {} for trials {}-{}"
+                .format(time, trials[0], trials[0]))
+        for i in range(trials[0], trials[1]+1):
+            self.world[i][time] = 0
+        return True
+    
+
+    def reset(self, max_time=-1, n_trials=-1, rewards=[], verbose=False):
+        max_time = self.max_time if max_time == -1 else max_time
+        n_trials = self.n_trials if n_trials == -1 else n_trials
+
+        if verbose:
+            print("Reseting world with max_time as {} and {} trials"
+                .format(max_time, n_trials))
+        self.world = np.zeros((n_trials, max_time))
+
+        for r, t, trials in rewards:
+            self.add_reward(r, t, trials, verbose=verbose)
+    
+
+    def run_TD(self, ALPHA=1, GAMMA=1):
+        value = np.zeros((self.n_trials+1, self.max_time))
+        prediction_error = np.zeros(self.world.shape)
+
+        for trial in range(self.n_trials):
+            for t in range(self.max_time):
+                prediction_error[trial][t] = self.world[trial][t] - value[trial][t]
+                value[trial+1][t] = value[trial][t] + ALPHA * (self.world[trial][t+1] + GAMMA * value[trial][t+1] - value[trial][t])
+
+        return value[1:], prediction_error
+    
